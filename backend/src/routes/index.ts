cd878071dce54105b348cf4777615bab10ccb443
Gabriel Ramos <luisgabrielramosrobles@gmail.com>
2025-07-04 15:43:20 -0400

    Created permission routes and tests

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,25 +1,16 @@
 import express from 'express'
 
+import { authenticateToken } from '@/middlewares/authentication'
+
 import authRouter from './auth'
+import permissionRouter from './permission'
 
 const mainRouter = express.Router()
 
 mainRouter.use('/auth', authRouter)
 
+// Authentication Required
+mainRouter.use(authenticateToken)
+mainRouter.use('/permissions', permissionRouter)
+
 export default mainRouter
-
-// src/app.ts
-import express from 'express'
-
-import { BASE_URL } from './config/env'
-import mainRouter from './routes/mainRouter'
-
-const app = express()
-
-app.use(express.json())
-app.use(mainRouter)
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running at http://${BASE_URL}:${PORT}`)
-})
